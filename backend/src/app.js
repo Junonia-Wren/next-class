ea853c4b0f89ea644cf3d8aae4a6b07ba1332b50
Junonia Wren <wrenjunonia@gmail.com>
2025-11-09 01:57:39 -0600

    Agrega login, registro y consulta de horarios por alumno

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,8 @@ import morgan from "morgan";
 import "./database.js";
 
 import scheduleRoutes from "./routes/schedule.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
 
 const app = express();
 
@@ -14,7 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 app.use("/api/schedules", scheduleRoutes);
-
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
 
 
 export default app; 
--- backend/src/models/schedule.model.js
+++ backend/src/models/schedule.model.js
@@ -1,23 +1,38 @@
 import { model, Schema } from "mongoose";
 
 const scheduleSchema = new Schema({
-
-    schedule_id:{
-    type: String,
-    required: true,
-    unique: true
-},
-    subject: String,//Materia
+    schedule_id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    area: {
+        type: String,
+        enum: ["DSM", "EVND"],
+        required: true
+    },
+    nivel: {
+        type: String,
+        enum: ["Técnico", "Ingeniería"],
+        required: true
+    },
+    grupo: {
+        type: String,
+        required: true
+    },
+    subject: String,
     teacher: String,
-    classroom: String,//salon
-    day: String,
+    classroom: String,
+    day: {
+        type: String,
+        enum: ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes"]
+    },
     startTime: String,
     endTime: String,
     roomImage: String
-},
-{
-    versionKey:false,
-    timestamps:true
+}, {
+    versionKey: false,
+    timestamps: true
 });
 
 export default model("schedule", scheduleSchema);
